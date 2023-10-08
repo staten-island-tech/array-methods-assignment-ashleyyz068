621ced95b9fb3254c26433fe6799c6e29d8bbc79
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-10-07 20:05:31 -0400

    day1

---
+++ app.js
@@ -0,0 +1,34 @@
+const deserts = [
+    {
+        desertName: "macarons",
+        countryOrigin: "France",
+        baked: true, 
+        price: 2,
+        storesSold: ("I Love Macarons", "Macaron World"),
+        posFlavors: ("chocolate", "pastascho")
+    },
+    {
+        desertName: "icecream",
+        countryOrigin: "China",
+        baked: false, 
+        price: 1,
+        storesSold: ("Sprinkle City", "Swirls 'n Smiles"),
+        posFlavors: ("cookie dough", "vanilla")
+    },
+    {
+        desertName: "jello",
+        countryOrigin: "New York",
+        baked: false, 
+        price: 0.50,
+        storesSold: ("target", "walmart"),
+        posFlavors: ("cherry", "cheesecake")
+    },
+    {
+        desertName: "churros",
+        countryOrigin: "Spain",
+        baked: false, 
+        price: 1,
+        storesSold: ("Churro-ville", "Churro Palace"),
+        posFlavors: ("Lava cake", "chocolate")
+    }
+]
