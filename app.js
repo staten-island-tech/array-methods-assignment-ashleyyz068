e2ae6752ea4554af4be964cd707c7a86048a7e61
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-10-11 09:24:26 -0400

    oct11

--- app.js
+++ app.js
@@ -4,31 +4,34 @@ const deserts = [
         countryOrigin: "France",
         baked: true, 
         price: 2,
-        storesSold: ("I Love Macarons", "Macaron World"),
-        posFlavors: ("chocolate", "pastascho")
+        storesSold: ["I Love Macarons", "Macaron World", "Macaron Majesty"],
+        posFlavors: ["chocolate", "pastascho"]
     },
     {
         desertName: "icecream",
         countryOrigin: "China",
         baked: false, 
         price: 1,
-        storesSold: ("Sprinkle City", "Swirls 'n Smiles"),
-        posFlavors: ("cookie dough", "vanilla")
+        storesSold: ["Sprinkle City", "Swirls 'n Smiles", "Scooplicity"],
+        posFlavors: ["cookie dough", "vanilla"]
     },
     {
         desertName: "jello",
         countryOrigin: "New York",
         baked: false, 
         price: 0.50,
-        storesSold: ("target", "walmart"),
-        posFlavors: ("cherry", "cheesecake")
+        storesSold: ["target", "walmart", "keyfoods"],
+        posFlavors: ["cherry", "cheesecake"]
     },
     {
         desertName: "churros",
         countryOrigin: "Spain",
         baked: false, 
         price: 1,
-        storesSold: ("Churro-ville", "Churro Palace"),
-        posFlavors: ("Lava cake", "chocolate")
+        storesSold: ["Churro-ville", "Churro Palace", "Sweet churros"],
+        posFlavors: ["Lava cake", "chocolate"]
     }
 ]
+
+const names = deserts.filter((desert)=> desert.desertName); 
+console.log(names)
